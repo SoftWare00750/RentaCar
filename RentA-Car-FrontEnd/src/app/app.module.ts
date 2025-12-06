cfb610e3bef18c5ad878777bf9d1b979fc48bfd1
SoftWare00750 <tunesbaby7175@gmail.com>
2025-12-06 06:14:24 +0000

    commit

--- RentA-Car-FrontEnd/src/app/app.module.ts
+++ RentA-Car-FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import  {HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtModule } from '@auth0/angular-jwt';
