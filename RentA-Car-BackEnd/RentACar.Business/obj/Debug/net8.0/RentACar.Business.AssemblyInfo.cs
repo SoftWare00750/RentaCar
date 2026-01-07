db9d26d5845aaa6a99bdf2197393b105d6575afd
Coding <150249195+Dammy-wise@users.noreply.github.com>
2026-01-07 16:27:23 +0100

    commit16

--- RentA-Car-BackEnd/RentACar.Business/obj/Debug/net8.0/RentACar.Business.AssemblyInfo.cs
+++ RentA-Car-BackEnd/RentACar.Business/obj/Debug/net8.0/RentACar.Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e825751b126668b883ae0c57a744567739be373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484f450fad5aa1a8951f23eb345ed232a4468025")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RentA-Car-BackEnd/RentACar.Core/obj/Debug/net8.0/RentACar.Core.AssemblyInfo.cs
+++ RentA-Car-BackEnd/RentACar.Core/obj/Debug/net8.0/RentACar.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e825751b126668b883ae0c57a744567739be373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484f450fad5aa1a8951f23eb345ed232a4468025")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
