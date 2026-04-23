ec1b6402382226c6b96144399d6860eedca35573
Coding <150249195+Dammy-wise@users.noreply.github.com>
2026-04-23 21:43:22 +0100

    commit23

--- RentA-Car-FrontEnd/src/app/services/color.service.ts
+++ RentA-Car-FrontEnd/src/app/services/color.service.ts
@@ -1,24 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
 import { ResponseModel } from '../models/responseModel';
 import { SingleResponseModel } from '../models/singleResponseModel';
-import { User } from '../models/user';
+import { Color } from '../models/color';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class ColorService {
   apiUrl = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) {}
 
-  getbyid(userId: number): Observable<SingleResponseModel<User>> {
-    return this.httpClient.get<SingleResponseModel<User>>(this.apiUrl + 'users/getbyid?userId=' + userId);
+  getColors(): Observable<ListResponseModel<Color>> {
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl + 'colors/getall');
   }
 
-  updateInfos(user: User): Observable<ResponseModel> {
-    return this.httpClient.put<ResponseModel>(this.apiUrl + 'users/updated', user);
+  getById(id: number): Observable<SingleResponseModel<Color>> {
+    return this.httpClient.get<SingleResponseModel<Color>>(this.apiUrl + 'colors/getbyid?colorId=' + id);
+  }
+
+  addColor(color: Color): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(this.apiUrl + 'colors/add', color);
+  }
+
+  updateColor(color: Color): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(this.apiUrl + 'colors/updated', color);
+  }
+
+  deleteColor(color: Color): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(this.apiUrl + 'colors/delete', color);
   }
 }
